e76f8647ae10d53d1d74bc7d2b545e7bb53bc867
Maura <mauagui2000@gmail.com>
2023-09-13 23:45:29 -0600

    Programación de los controladores Estudiante y Materia

--- PortalEstudiantil.WebAPI/Controllers/EstudianteController.cs
+++ PortalEstudiantil.WebAPI/Controllers/EstudianteController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PortalEstudiantil.WebAPI.Auth;
+using Microsoft.AspNetCore.Authorization;
+using System.Text.Json;
+using PortalEstudiantil.EntidadesDeNegocio;
+using PortalEstudiantil.LogicaDeNegocio;
+using PortalEstudiantil.AccesoADatos;
 
 namespace PortalEstudiantil.WebAPI.Controllers
 {
@@ -7,5 +13,108 @@ namespace PortalEstudiantil.WebAPI.Controllers
     [ApiController]
     public class EstudianteController : ControllerBase
     {
+        private EstudianteBL estudianteBL = new EstudianteBL();
+
+        // Codigo para agregar la seguridad JWT
+        private readonly IJwtAuthenticationServices authService;
+        public EstudianteController(IJwtAuthenticationServices pAuthService)
+        {
+            authService = pAuthService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Estudiante estudiante)
+        {
+            try
+            {
+                await estudianteBL.CrearAsync(estudiante);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] object pEstudiante)
+        {
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strEstudiante = JsonSerializer.Serialize(pEstudiante);
+            Estudiante estudiante = JsonSerializer.Deserialize<Estudiante>(strEstudiante, option);
+            if (estudiante.Id == id)
+            {
+                await estudianteBL.ModificarAsync(estudiante);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(short id)
+        {
+            try
+            {
+                Estudiante estudiante = new Estudiante();
+                estudiante.Id = id;
+                await estudianteBL.EliminarAsync(estudiante);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Estudiante> Get(short id)
+        {
+            Estudiante estudiante = new Estudiante();
+            estudiante.Id = id;
+            return await estudianteBL.ObtenerPorIdAsync(estudiante);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Estudiante>> Get()
+        {
+            return await estudianteBL.ObtenerTodosAsync();
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Estudiante>> Buscar([FromBody] object pEstudiante)
+        {
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strEstudiante = JsonSerializer.Serialize(pEstudiante);
+            Estudiante estudiante = JsonSerializer.Deserialize<Estudiante>(strEstudiante, option);
+            return await estudianteBL.BuscarAsync(estudiante);
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Estudiante>> BuscarGrado([FromBody] object pEstudiante)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strEstudiante = JsonSerializer.Serialize(pEstudiante);
+            Estudiante estudiante = JsonSerializer.Deserialize<Estudiante>(strEstudiante, option);
+            var estudiantes = await estudianteBL.BuscarIncluirGradoAsync(estudiante);
+            estudiantes.ForEach(s => s.Grado.estudiante = null); // Evitar la redundacia de datos
+            return estudiantes;
+        }
+
+
+        [HttpPost("Buscar")]
+        public async Task<List<Estudiante>> BuscarTurno([FromBody] object pEstudiante)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strEstudiante = JsonSerializer.Serialize(pEstudiante);
+            Estudiante estudiante = JsonSerializer.Deserialize<Estudiante>(strEstudiante, option);
+            var estudiantes = await estudianteBL.BuscarIncluirTurnoAsync(estudiante);
+            estudiantes.ForEach(s => s.Turno.Estudiante = null); // Evitar la redundacia de datos
+            return estudiantes;
+        }
     }
 }
--- PortalEstudiantil.WebAPI/Controllers/MateriaController.cs
+++ PortalEstudiantil.WebAPI/Controllers/MateriaController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PortalEstudiantil.AccesoADatos;
+using PortalEstudiantil.EntidadesDeNegocio;
+using PortalEstudiantil.LogicaDeNegocio;
+using PortalEstudiantil.WebAPI.Auth;
+using System.Text.Json;
 
 namespace PortalEstudiantil.WebAPI.Controllers
 {
@@ -7,5 +12,83 @@ namespace PortalEstudiantil.WebAPI.Controllers
     [ApiController]
     public class MateriaController : ControllerBase
     {
+        private MateriaBL materiaBL = new MateriaBL();
+
+        // Codigo para agregar la seguridad JWT
+        private readonly IJwtAuthenticationServices authService;
+        public MateriaController(IJwtAuthenticationServices pAuthService)
+        {
+            authService = pAuthService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Materia materia)
+        {
+            try
+            {
+                await materiaBL.CrearAsync(materia);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] object pMateria)
+        {
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strMateria = JsonSerializer.Serialize(pMateria);
+            Materia materia = JsonSerializer.Deserialize<Materia>(strMateria, option);
+            if (materia.Id == id)
+            {
+                await materiaBL.ModificarAsync(materia);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(short id)
+        {
+            try
+            {
+                Materia materia = new Materia();
+                materia.Id = id;
+                await materiaBL.EliminarAsync(materia);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Materia> Get(short id)
+        {
+            Materia materia = new Materia();
+            materia.Id = id;
+            return await materiaBL.ObtenerPorIdAsync(materia);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Materia>> Get()
+        {
+            return await materiaBL.ObtenerTodosAsync();
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Materia>> Buscar([FromBody] object pMateria)
+        {
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strMateria = JsonSerializer.Serialize(pMateria);
+            Materia materia = JsonSerializer.Deserialize<Materia>(strMateria, option);
+            return await materiaBL.BuscarAsync(materia);
+        }
     }
 }
